53bc0c8d9edbcd70ad048be026b91dd5d6ea1914
Flavius Lacatusu <flacatus@redhat.com>
2024-01-16 11:41:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function run_web_site(port) {
 	app.use(express.static('html'));
 	app.use("/", express.Router());   
 	app.listen(port, function () {
-		console.log("Static site hosted on port", port);
+		console.log("Statisssc site hosted on port", port);
 	});
 } 
 run_web_site(8080) 
